adb4e6d2a87db6355a60fbad56cc588ed7f56bc6
Steven Phan <ptviet@live.com>
2018-09-18 12:17:20 +1000

    Initial Commit

---
+++ GoalPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Steven Phan on 18/9/18.
+//  Copyright © 2018 SP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
